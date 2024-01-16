9098aae545302faee537459350863b27f946c72d
Pauline Walhout <86212@glr.nl>
2024-01-16 16:31:55 +0100

    conditonal rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 
-import { CORE_CONCEPTS } from "./data.js"
-import Header from "./components/header/Header.jsx"
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js"
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState()
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
-    setSelectedTopic(selectedButton)
-}
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -19,34 +33,24 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={() => handleClick("components")}>Components</TabButton>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
             <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
             <TabButton onClick={() => handleClick("props")}>Props</TabButton>
             <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
-          {!selectedTopic ? ( 
-            <p>Please select a topic.</p> 
-            ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>
-                  {EXAMPLES[selectedTopic].code}
-                </code>
-              </pre>
-            </div>
-            ) }
         </section>
+        {tabContent}
       </main>
     </div>
   );

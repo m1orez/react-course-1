11b7c5851bb673912d7fa5f67bb5e71fb74929ff
Pauline Walhout <86212@glr.nl>
2024-01-16 16:26:37 +0100

    conditional statements

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TabButton from "./components/TabButton.jsx";
 import { EXAMPLES } from "./data.js"
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components')
+  const [ selectedTopic, setSelectedTopic ] = useState()
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton)
@@ -33,15 +33,19 @@ function App() {
             <TabButton onClick={() => handleClick("props")}>Props</TabButton>
             <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {!selectedTopic ? ( 
+            <p>Please select a topic.</p> 
+            ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+            ) }
         </section>
       </main>
     </div>
